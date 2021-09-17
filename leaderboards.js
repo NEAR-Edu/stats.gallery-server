1d4a182802737c40854d37e6db20f20bcdf0d7ec
Jacob <encody@users.noreply.github.com>
2021-09-17 11:43:40 -0500

    leaderboard blacklisting nfeco* and nfendowment* accounts

--- leaderboards.js
+++ leaderboards.js
@@ -272,6 +272,8 @@ class LeaderboardCache {
       where balance is not null and score is not null
         and account_id not like '%.lockup.near'
         and account_id not like '%.poolv1.near'
+        and account_id not like 'nfeco__.near'
+        and account_id not like 'nfendowment__.near'
       order by account.balance desc
       limit 20
     `);
@@ -284,6 +286,8 @@ class LeaderboardCache {
       where balance is not null and score is not null
         and account_id not like '%.lockup.near'
         and account_id not like '%.poolv1.near'
+        and account_id not like 'nfeco__.near'
+        and account_id not like 'nfendowment__.near'
       order by account.score desc
       limit 20
     `);
