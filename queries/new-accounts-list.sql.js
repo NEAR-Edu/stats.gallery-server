c4595f9695a4e97c52dcb5f4594d0d6edefb758c
Jacob <encody@users.noreply.github.com>
2021-09-27 13:57:50 -0500

    ensure score exists when listing new accounts

--- queries/new-accounts-list.sql.js
+++ queries/new-accounts-list.sql.js
@@ -5,6 +5,8 @@ module.exports = () => {
     select account_id, created_at_block_timestamp as block_timestamp
     from account
     where created_at_block_timestamp is not null
+      and score is not null
+      and balance is not null
     order by created_at_block_timestamp desc
     limit 10
   `;
