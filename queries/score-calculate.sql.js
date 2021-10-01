e73f01cac4de6e4280d267ace1d03d258a8f65cb
Mario Reder <mreder1289@gmail.com>
2021-10-01 06:21:54 +0200

    optimize score calculation query

--- queries/score-calculate.sql.js
+++ queries/score-calculate.sql.js
@@ -4,28 +4,24 @@ module.exports = params => {
   return sql`
     select coalesce(sum(
       case
-        when action_kind = 'TRANSFER'
-          and signer_account_id = ${params.account_id} then 10
-        when action_kind = 'TRANSFER'
-          and tx.receiver_account_id = ${params.account_id} then 2
-        when action_kind = 'CREATE_ACCOUNT'
-          and signer_account_id = ${params.account_id} then 50
-        when action_kind = 'FUNCTION_CALL'
-          and signer_account_id = ${params.account_id} then 10
-        when action_kind = 'DEPLOY_CONTRACT'
-          and signer_account_id = ${params.account_id} then 100
+        when a.action_kind = 'TRANSFER' and signer_account_id = ${params.account_id} then 10
+        when a.action_kind = 'TRANSFER' and receiver_account_id = ${params.account_id} then 2
+        when a.action_kind = 'CREATE_ACCOUNT' then 50
+        when a.action_kind = 'FUNCTION_CALL' then 10
+        when a.action_kind = 'DEPLOY_CONTRACT' then 100
         else 0
       end
-    ), 0) as result
-    from (
-      select *
-      from transactions
-      where (transactions.signer_account_id = ${params.account_id}
-        or transactions.receiver_account_id = ${params.account_id})
-      order by block_timestamp desc
-    ) tx
-    inner join receipts on tx.converted_into_receipt_id = receipts.receipt_id
-    left outer join transaction_actions on tx.transaction_hash = transaction_actions.transaction_hash
-    limit 2000
+    ), 0) as result from
+      (select action_kind, signer_account_id, tx.receiver_account_id from
+        (select *
+          from transactions
+          where (transactions.signer_account_id = ${params.account_id}
+          or transactions.receiver_account_id = ${params.account_id})
+          order by block_timestamp desc
+        ) tx
+        inner join receipts on tx.converted_into_receipt_id = receipts.receipt_id
+        left outer join transaction_actions on tx.transaction_hash = transaction_actions.transaction_hash
+        where signer_account_id = ${params.account_id} or tx.receiver_account_id = ${params.account_id}
+      ) a    
   `;
 };
