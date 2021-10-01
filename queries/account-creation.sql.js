bb2678482cbeb8acc706e21240a06556e7fbb6a7
Mario Reder <mreder1289@gmail.com>
2021-10-01 06:22:57 +0200

    fix thrown error on account creation query

--- queries/account-creation.sql.js
+++ queries/account-creation.sql.js
@@ -7,6 +7,7 @@ module.exports = params => {
     join action_receipt_actions r
       on a.created_by_receipt_id = r.receipt_id
     where a.account_id = ${params.account_id}
+      and a.created_by_receipt_id is not null
     limit 1
   `;
 };
