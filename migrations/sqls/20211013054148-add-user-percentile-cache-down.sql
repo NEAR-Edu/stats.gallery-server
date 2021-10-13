41e3e683767a6941f30931efe7cfed5232933f48
Melbex De Leon <melbex11@gmail.com>
2021-10-13 16:10:53 +0800

    use plural form in naming the tables

--- migrations/sqls/20211013054148-add-user-percentile-cache-down.sql
+++ migrations/sqls/20211013054148-add-user-percentile-cache-down.sql
@@ -1,1 +1,1 @@
-DROP TABLE user_percentile;
+DROP TABLE user_percentiles;
--- migrations/sqls/20211013054148-add-user-percentile-cache-up.sql
+++ migrations/sqls/20211013054148-add-user-percentile-cache-up.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user_percentile (
+CREATE TABLE user_percentiles (
   percentile_name   varchar(200) NOT NULL PRIMARY KEY,
   percentile        numeric(16, 16) default 0
 );
