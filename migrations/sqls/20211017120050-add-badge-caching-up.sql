b3eabd68ac9bb68cf5973a990f7f9632d523db62
Melbex De Leon <melbex11@gmail.com>
2021-10-17 21:12:28 +0800

    draft

---
+++ migrations/sqls/20211017120050-add-badge-caching-up.sql
@@ -0,0 +1,23 @@
+create table badge_group (
+  id uuid primary key,
+  group_name text
+);
+
+create unique index uniqe_badge_group_name on badge_group using btree (group_name);
+
+create table badge (
+  id uuid primary key,
+  badge_group_id uuid,
+  badge_name text not null,
+  badge_description text not null,
+  rarity_fraction numeric(16,16) default 0,
+  href text default null,
+  required_value numeric
+);
+
+alter table badge
+  add constraint fk_badge_group_id
+  foreign key (badge_group_id)
+  references badge_group (id)
+    on delete cascade
+    on update cascade;
