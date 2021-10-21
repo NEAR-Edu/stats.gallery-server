ee4818057f8a60ac3cba0de3631143c98c2961ba
Jacob <encody@users.noreply.github.com>
2021-10-21 00:52:55 -0500

    enforce formatting

--- src/queries/gas-tokens-spent.sql.ts
+++ src/queries/gas-tokens-spent.sql.ts
@@ -4,12 +4,20 @@ import { Params } from './Params';
 export default (params: Params) => {
   const conditions = [sql`signer_account_id = ${params.account_id}`];
 
-  if (params.after_block_timestamp !== undefined && params.after_block_timestamp > 0) {
+  if (
+    params.after_block_timestamp !== undefined &&
+    params.after_block_timestamp > 0
+  ) {
     conditions.push(sql`block_timestamp >= ${params.after_block_timestamp}`);
   }
 
-  if (params.before_block_timestamp !== undefined && params.before_block_timestamp > 0) {
-    conditions.push(sql`transactions.block_timestamp <= ${params.before_block_timestamp}`);
+  if (
+    params.before_block_timestamp !== undefined &&
+    params.before_block_timestamp > 0
+  ) {
+    conditions.push(
+      sql`transactions.block_timestamp <= ${params.before_block_timestamp}`,
+    );
   }
 
   return sql`
