be22f5abe1687002066fd59b58c0e055bdda9b11
Melbex De Leon <melbex11@gmail.com>
2021-11-09 13:17:08 +0800

    add migration for caching transactions

---
+++ migrations/sqls/20211106113018-add-caching-for-leaderboards-down.sql
@@ -0,0 +1,3 @@
+alter table last_update drop column cron_name;
+drop table on_chain_transaction;
+drop type execution_outcome_status;
---
+++ migrations/sqls/20211106113018-add-caching-for-leaderboards-up.sql
@@ -0,0 +1,28 @@
+alter table last_update
+  add column cron_name text unique;
+
+-- add name to the existing only column: accounts cache
+update last_update set cron_name = 'ACCOUNTS_CACHE';
+
+create type execution_outcome_status as ENUM (
+  'UNKNOWN',
+  'FAILURE',
+  'SUCCESS_VALUE',
+  'SUCCESS_RECEIPT_ID'
+);
+
+create table on_chain_transaction (
+  transaction_hash text not null primary key,
+  index_in_chunk int4 not null,
+  block_timestamp numeric(20,0) not null,
+  signer_account_id text not null,
+  signer_public_key text not null,
+  nonce numeric(20, 0) not null,
+  receiver_account_id text not null,
+  signature text not null,
+  status execution_outcome_status not null,
+  receipt_conversion_gas_burnt numeric(20,0),
+  receipt_conversion_tokens_burnt numeric(45,0)
+);
+
+create index on_chain_transaction_block_timestamp on on_chain_transaction using btree (block_timestamp);
