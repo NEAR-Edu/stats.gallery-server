e42d98afbe447996beabd8add0c0c1368aad1659
Melbex De Leon <melbex11@gmail.com>
2022-01-22 16:33:55 +0800

    make script idempotent by adding a downgrade script

--- migrations/sqls/20220121104939-add-pk-to-account-table-down.sql
+++ migrations/sqls/20220121104939-add-pk-to-account-table-down.sql
@@ -1,1 +1,25 @@
-/*Intentionally left blank*/
+alter table account_badge
+  drop constraint fk_account_badge_account_id,
+  add column temp_account_id text default '';
+
+alter table account drop constraint account_account_id_unique;
+
+update account_badge 
+  set temp_account_id = account.account_id
+from account
+where account_badge.account_id = account.id;
+
+alter table account_badge drop column account_id;
+
+alter table account_badge rename column temp_account_id to account_id; 
+
+alter table account drop column id;
+
+alter table account add constraint account_pkey primary key (account_id);
+
+alter table account_badge
+  add constraint fk_account_badge_account_id
+  foreign key (account_id)
+  references account (account_id)
+    on delete cascade
+    on update cascade;
