bb05edfe61457b6bcf5e9cd5d130c6d1d4f03623
Melbex De Leon <melbex11@gmail.com>
2022-02-13 10:15:51 +0800

    fix badge transfer

--- src/services/badge/transfer.ts
+++ src/services/badge/transfer.ts
@@ -44,76 +44,30 @@ export default (spec: TransferBadgeSpec): BadgeService => {
     });
   };
 
-  const getAccountTokenTransfer = async (
+  const isBadgeAttained = async (
     accountId: string,
-    badgeNames: string[],
-  ): Promise<readonly QueryResultRow[]> => {
-    try {
-      const result = await statsGalleryCache.many(
-        sql`
-        select
-          account_badge.attained_value,
-          badge.badge_name,
-          badge.bagde_description,
-          badge.required_value,
-          badge.rarity_fraction,
-          badge.badge_group_id
-        from
-          account_badge
-        inner join
-          badge
-        on
-          badge.badge_group_id = account_badge.badge_group_id
-        inner join
-          account
-        on
-          account.id = account_badge.account_id
-        where
-          account.account_id = ${accountId} and badge.badge_name in ${sql.join(
-          badgeNames,
-          sql`,`,
-        )}`,
-      );
-      return result;
-    } catch (error) {
-      console.error(error);
-      const empty: readonly QueryResultRow[] = [];
-      return empty;
-    }
-  };
-
-  const isBadgeAttained = async (accountId: string): Promise<boolean> => {
+  ): Promise<readonly any[]> => {
     const redisKey = serviceName + '_' + accountId;
     const cachedValue = await cacheLayer.get(redisKey);
     if (cachedValue !== null) {
-      return cachedValue === 'true';
+      return JSON.parse(cachedValue) as readonly any[];
     }
 
     const transferBadges = await statsGalleryCache.query(sql`
       select
         badge.badge_name as badge_name,
-        badge.required_level as level
+        badge.required_value,
+        badge.badge_group_id,
+        badge.badge_description,
+        badge.rarity_fraction
       from
         badge
       inner join badge_group on badge.badge_group_id = badge_group.id
       where
         badge_group.function_name = 'badge-transfer'
       order by
-        required_level asc;
+        required_value asc;
     `);
-
-    const badgeNames: string[] = [];
-    transferBadges.rows.forEach(tb => {
-      badgeNames.push(tb.badge_name as string);
-    });
-
-    const badgesAttained = await getAccountTokenTransfer(accountId, badgeNames);
-
-    if (badgesAttained.length > 0) {
-      await cacheLayer.set(redisKey, JSON.stringify(badgesAttained));
-
-      return true;
-    }
 
     const result = await indexerPool.one(
       badgeTransferSql({ account_id: accountId }),
@@ -123,48 +77,46 @@ export default (spec: TransferBadgeSpec): BadgeService => {
 
     if (performedTransfer) {
       const attainedBadges = [];
-      const valuesToInsert = [];
+      const transfers = Number(result!.result) || 0;
       for (const badge of transferBadges.rows) {
-        const transfers = Number(result!.result) || 0;
-        if (transfers >= Number(badge.level)) {
+        if (transfers >= Number(badge.required_value)) {
           attainedBadges.push({
             attained_value: transfers,
             badge_group_id: badge.badge_group_id,
             badge_name: badge.badge_name,
-            bagde_description: badge.bagde_description,
+            badge_description: badge.badge_description,
             required_value: badge.required_value,
             rarity_fraction: badge.rarity_fraction,
-          });
-
-          valuesToInsert.push({
-            badge_group_id: badge.badge_group_id,
-            attained_value: transfers,
-            account_id: accountId,
           });
         }
       }
 
-      await statsGalleryCache.query(
-        sql`insert into account_badge (badge_group_id, attained_value, account_id)
-        select 
-          *
-        from ${sql.unnest(valuesToInsert as readonly any[], [
-          'uuid',
-          'numeric',
-          'uuid',
-        ])}`,
-      );
+      if (attainedBadges.length > 0) {
+        await statsGalleryCache.query(sql`
+          insert
+            into
+          account_badge (badge_group_id, attained_value, account_id)
+          values (${attainedBadges[0].badge_group_id}, ${transfers}, (select id from account where account_id = ${accountId}))
+          on conflict on constraint account_badge_badge_group_account_idx
+          do
+            update set attained_value = ${transfers}
+        `);
 
-      await cacheLayer.set(redisKey, JSON.stringify(attainedBadges), {
-        EX: 86_400,
-      });
+        await cacheLayer.set(redisKey, JSON.stringify(attainedBadges), {
+          EX: 3_600,
+        });
+
+        return attainedBadges as readonly any[];
+      }
+
+      return [] as readonly any[];
     }
 
     // we set an expiration period for when the value we get is false as to give a chance
     // for redis to be replinished just in case the user completes the badge in the future
-    await cacheLayer.set(redisKey, JSON.stringify({}), { EX: 600 });
+    await cacheLayer.set(redisKey, JSON.stringify([]), { EX: 600 });
 
-    return performedTransfer;
+    return [] as readonly any[];
   };
 
   return Object.freeze({
